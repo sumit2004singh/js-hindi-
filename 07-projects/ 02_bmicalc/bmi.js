8f5131cf5cb4e4c89a1faafcc4dac7ea7cda1533
sumit2004singh <sumitsingh981156@gmail.com>
2024-09-07 19:06:45 +0000

    01 and 02 project

---
+++ 07-projects/ 02_bmicalc/bmi.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
+    if(height === "" || height < 0 || isNaN(height)){
+        result.innerHTML = "please give a valid height"
+    }
+    if(weight === "" || weight < 0 || isNaN(weight)){
+        result.innerHTML = "please give a valid weight"
+    } else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        //show the result
+        result.innerHTML = `<span>${bmi}</span>`
+    }
+})
